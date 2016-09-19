7c7f26e67a59305447f6ce19e3b5edd8414f043a
Drake Ballard <drakeballard@gmail.com>
2016-09-18 19:01:49 -0500

    adding firebase, jquery, bootstrap and moment

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 alert('The train is coming 1, 2 ,3');
+
+
+//firebase application
+// Initialize Firebase
+// var config = {
+//   apiKey: "AIzaSyDChpuTErGfXUtRSDAAWOAMSN8fAoBGb9Y",
+//   authDomain: "train-schedule-61b08.firebaseapp.com",
+//   databaseURL: "https://train-schedule-61b08.firebaseio.com",
+//   storageBucket: "train-schedule-61b08.appspot.com",
+//   messagingSenderId: "26945234726"
+// };
+// firebase.initializeApp(config);

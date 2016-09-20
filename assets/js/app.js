d95f38e6e57bf8391fa7953a22537f5addb37d86
Drake Ballard <drakeballard@gmail.com>
2016-09-20 18:14:10 -0500

    uncommented my firebase app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,14 @@
-alert('The train is coming 1, 2 ,3');
+//The JS reference is no longer needed
+// alert('The train is coming 1, 2 ,3');
 
 
-//firebase application
-// Initialize Firebase
-// var config = {
-//   apiKey: "AIzaSyDChpuTErGfXUtRSDAAWOAMSN8fAoBGb9Y",
-//   authDomain: "train-schedule-61b08.firebaseapp.com",
-//   databaseURL: "https://train-schedule-61b08.firebaseio.com",
-//   storageBucket: "train-schedule-61b08.appspot.com",
-//   messagingSenderId: "26945234726"
-// };
-// firebase.initializeApp(config);
+firebase application
+Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDChpuTErGfXUtRSDAAWOAMSN8fAoBGb9Y",
+  authDomain: "train-schedule-61b08.firebaseapp.com",
+  databaseURL: "https://train-schedule-61b08.firebaseio.com",
+  storageBucket: "train-schedule-61b08.appspot.com",
+  messagingSenderId: "26945234726"
+};
+firebase.initializeApp(config);
